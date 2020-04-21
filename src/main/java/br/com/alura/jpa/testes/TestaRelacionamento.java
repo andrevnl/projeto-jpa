3acfe3fd0885d8a277075c456d4ff24c539559ab
andrevnl <andrevnl@hotmail.com>
2020-04-21 18:46:02 -0300

    Criando testes para entender a JPA

---
+++ src/main/java/br/com/alura/jpa/testes/TestaRelacionamento.java
@@ -0,0 +1,35 @@
+package br.com.alura.jpa.testes;
+
+import br.com.alura.jpa.modelo.Conta;
+import br.com.alura.jpa.modelo.Movimentacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TestaRelacionamento {
+
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        conta.setTitular("Leonardo");
+        conta.setNumero(1234);
+        conta.setAgencia(4321);
+
+        Movimentacao movimentacao = new Movimentacao();
+        movimentacao.setData(LocalDateTime.now());
+        movimentacao.setDescricao("Churrascaria");
+        movimentacao.setValor(new BigDecimal(200.0));
+        movimentacao.setConta(conta);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(conta);
+        em.persist(movimentacao);
+        em.getTransaction().commit();
+        em.close();
+    }
+}
